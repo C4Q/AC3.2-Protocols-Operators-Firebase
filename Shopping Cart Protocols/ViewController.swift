f8b9355b714b6d098bb1c45709e4b572e97065bc
Louis Tur <louis.tur@gmail.com>
2017-01-31 13:17:28 -0500

    morning code from 1/31

--- Shopping Cart Protocols/ViewController.swift
+++ Shopping Cart Protocols/ViewController.swift
@@ -12,8 +12,9 @@ import Firebase
 
 class ViewController: UIViewController {
   
-  let databaseReference = FIRDatabase.database().reference().child("items")
+  let databaseReference = FIRDatabase.database().reference().child("shoppingCartItems")
   var databaseObserver: FIRDatabaseHandle?
+  var signInUser: FIRUser?
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -22,8 +23,29 @@ class ViewController: UIViewController {
     setupViewHierarchy()
     configureConstraints()
     
+    loginAnonymously()
     setObserver()
-    testAddingAnItem()
+    //    testAddingAnItem()
+  }
+  
+  private func loginAnonymously() {
+    
+    FIRAuth.auth()?.signInAnonymously(completion: { (user: FIRUser?, error: Error?) in
+      
+      if error != nil {
+        print("Error attempting to log in anonymously: \(error!)")
+      }
+      
+      if user != nil {
+        print("Signed in anonymously!")
+        self.signInUser = user
+        
+        print("About to Save an item")
+        self.testAddingAnItem()
+      }
+      
+    })
+    
   }
   
   private func setObserver() {
@@ -33,14 +55,15 @@ class ViewController: UIViewController {
   }
   
   func testAddingAnItem() {
-    let newItem = ShoppingCartItem(price: 28.00, name: "General Romance Novel, 2017: by Gabe", sku: 100000031, quantity: 3)
+    let newItem = ShoppingCartItem(price: 28.00, name: "Epic Romance Novel, 2017: by Gabe feat. Tom", sku: 100000031, quantity: 3)
     
     let newItemRef = databaseReference.childByAutoId()
     let newItemDetails: [String : AnyObject] = [
       "price" : newItem.price as AnyObject,
       "name" : newItem.name as AnyObject,
       "sku" : newItem.sku as AnyObject,
-      "quantity" : newItem.quantity as AnyObject
+      "quantity" : newItem.quantity as AnyObject,
+      "addedBy" : (self.signInUser?.uid ?? "Not Signed In/Semi Anon") as AnyObject
     ]
     newItemRef.setValue(newItemDetails)
   }
@@ -53,6 +76,6 @@ class ViewController: UIViewController {
     
   }
   
-
+  
 }
 
